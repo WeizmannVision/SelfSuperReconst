7bc2c7bd585a501a2852d6fdd4b846032249cf85
guyga <ggaziv@gmail.com>
2021-10-25 20:22:14 +0300

    Refactor and debug

--- setup_data.sh
+++ setup_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Author: Guy Gaziv
 
 set -e
 
@@ -14,45 +15,53 @@ cd data
 
 RELEASE_PATH=https://github.com/WeizmannVision/SelfSuperReconst/releases/download/v1/
 
+# 'fMRI on ImageNet' presented images at resolution of 112
 wget $RELEASE_PATH/images_112.npz
-wget $RELEASE_PATH/rgbd_112_from_224_large_png_uint8.npz
-wget -m -nd -A "sbj_*.npz" $RELEASE_PATH 
-# for sbj_num in 1 2 3 4 5
-# do
-#     wget $RELEASE_PATH/sbj_${sbj_num}.npz        
-# done
 
+# 'fMRI on ImageNet' presented images at resolution of 112 with their depth channel, 
+# which was computed on 224x224 images using MiDaS large model and saved as PNG uint8
+wget $RELEASE_PATH/rgbd_112_from_224_large_png_uint8.npz
+
+# 'fMRI on ImageNet' subject preprocessed fMRI data
+for sbj_num in 1 2 3 4 5
+do
+    wget $RELEASE_PATH/sbj_${sbj_num}.npz        
+done
+
+# MiDaS pretrained models for depth estimation from images
 wget https://github.com/intel-isl/MiDaS/releases/download/v2_1/model-f6b98070.pt
 wget https://github.com/intel-isl/MiDaS/releases/download/v2_1/model-small-70d6b9c8.pt
 
-wget $RELEASE_PATH/vgg19_depth_only_large_norm_within_img_best.pth.tar
-wget $RELEASE_PATH/vgg19_depth_only_norm_within_img_best.pth.tar
-
-mkdir -p imagenet/val
-mkdir -p imagenet_depth/val_depth_on_orig_small_png_uint8
-mkdir -p imagenet_depth/val_depth_on_orig_large_png_uint8
-mkdir imagenet_rgbd
-
+# ImageNet validation data
+mkdir imagenet
+cd imagenet
 filename=imagenet_val.tar.gz
-cd imagenet/val
 wget $RELEASE_PATH/$filename
 tar xvzf $filename
+rm -f $filename
+cd ..
 
-cd ../imagenet_depth/val_depth_on_orig_small_png_uint8
+# Depth estimated over ImageNet validation data using both small/large MiDaS model
+mkdir imagenet_depth
+cd imagenet_depth
 filename=val_depth_on_orig_small_png_uint8.tar.gz
 wget $RELEASE_PATH/$filename
 tar xvzf $filename
+rm -f $filename
 
-cd ../val_depth_on_orig_large_png_uint8
 filename=val_depth_on_orig_large_png_uint8.tar.gz
 wget $RELEASE_PATH/$filename
 tar xvzf $filename
+rm -f $filename
+cd ..
 
-cd ../imagenet_rgbd
-wget -r -A .pth.tar $RELEASE_PATH
-# wget $RELEASE_PATH/vgg16_depth_only_large_norm_within_img_best.pth.tar
-# wget $RELEASE_PATH/vgg16_depth_only_norm_within_img_best.pth.tar
-# wget $RELEASE_PATH/vgg19_depth_only_large_norm_within_img_best.pth.tar
-# wget $RELEASE_PATH/vgg19_depth_only_norm_within_img_best.pth.tar
+# Pretrained checkpoints optimized for ILSVRC from depth input or RGBD
+mkdir imagenet_rgbd
+cd imagenet_rgbd
+wget $RELEASE_PATH/vgg16_depth_only_large_norm_within_img_best.pth.tar
+wget $RELEASE_PATH/vgg16_depth_only_norm_within_img_best.pth.tar
+wget $RELEASE_PATH/vgg16_rgbd_large_norm_within_img_best.pth.tar
+wget $RELEASE_PATH/vgg19_depth_only_large_norm_within_img_best.pth.tar
+wget $RELEASE_PATH/vgg19_rgbd_large_norm_within_img_best.pth.tar
 
-cd ../..
+cd ..
