09d4186c65fe6aec473748285c664ad8bebd1ca0
Guy Gaziv <ggaziv@gmail.com>
2022-03-22 12:35:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.0',
     author="Guy Gaziv",
     author_email='guy.gaziv@weizmann.ac.il',
-    description="Self-Supervised RGBD Reconstruction from Brain Activity (Official PyTorch implementation)",
+    description="Self-Supervised RGBD Reconstruction From Brain Activity (Official PyTorch implementation)",
     long_description=open("README.md", "r", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     keywords="Self-Supervised Learning; Decoding; Encoding; fMRI Image Reconstruction; Classification; Vision; Depth Estimation",
